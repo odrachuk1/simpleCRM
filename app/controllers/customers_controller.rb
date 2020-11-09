126704c4afd52135b2b1fd5daf6cd690dc86d9dc
O. Drachuk <ksenorin@gmail.com>
2020-11-08 22:04:14 -0600

    Adds two views and routs

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,4 +2,10 @@ class CustomersController < ApplicationController
   def index
     @customers = Customer.all
   end
+  def alphabetized
+    @customers = Customer.order(:full_name)
+  end
+  def missing_email
+    @customers = Customer.where("email = ''")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: "customers#index"
+  get 'customers/missing_email'
+  get 'customers/alphabetized'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
